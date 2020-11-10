e35feccecec262c3d3bbae1d44ddfbbe9ef865da
an44ajh <an44ajh@gmail.com>
2020-11-10 16:30:05 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from pybo.models import Question, Answer
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
